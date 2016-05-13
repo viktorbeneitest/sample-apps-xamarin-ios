ad29afc1812d161f4950d39e3021b00eee2b91c9
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-05-13 17:40:03 +0200

    unit test

---
+++ CreditCardValidator.iOS.UnitTests/Tests.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace CreditCardValidator.iOS.UnitTests
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Pass ()
+		{
+			Assert.True (true);
+		}
+
+		[Test]
+		public void Fail ()
+		{
+			Assert.False (true);
+		}
+
+		[Test]
+		[Ignore ("another time")]
+		public void Ignore ()
+		{
+			Assert.True (false);
+		}
+	}
+}
