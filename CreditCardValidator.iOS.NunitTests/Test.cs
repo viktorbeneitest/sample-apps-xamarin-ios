23d1ee8155ca26152c41e6a6dd004f439ded8597
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-05-18 16:10:50 +0200

    nunit & nunit lite tests

---
+++ CreditCardValidator.iOS.NunitTests/Test.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CreditCardValidator.iOS.NunitTests
+{
+	[TestFixture]
+	public class TestsSample
+	{
+
+		[SetUp]
+		public void Setup ()
+		{
+		}
+
+
+		[TearDown]
+		public void Tear ()
+		{
+		}
+
+		[Test]
+		public void Pass ()
+		{
+			Console.WriteLine ("test1");
+			Assert.True (true);
+		}
+
+		[Test]
+		[Ignore ("another time")]
+		public void Ignore ()
+		{
+			Assert.True (false);
+		}
+
+		[Test]
+		public void Inconclusive ()
+		{
+			Assert.Inconclusive ("Inconclusive");
+		}
+	}
+
+}
+
